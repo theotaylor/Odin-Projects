abc85b490b1e65cf4ce6caed94bd35976047253e
theotaylor <theotaylor2000@gmail.com>
2024-07-30 13:25:43 -0400

    added game with buttons

--- exercises/rockpaperscissors/rps.js
+++ exercises/rockpaperscissors/rps.js
@@ -1,4 +1,24 @@
+let result = document.createElement("div");
+let score = document.createElement("div");
+let main = document.querySelector("main");
+main.appendChild(result);
+main.appendChild(score);
 
+let computerScore = 0;
+let humanScore = 0;
+
+updateScore();
+
+function checkWinner() {
+    if (computerScore === 5) result.textContent = `The computer won`;
+    if (humanScore === 5) result.textContent = `You win`;
+}
+
+
+
+function updateScore() {
+    score.textContent = `${humanScore} - ${computerScore}`;
+}
 
 function getComputerChoice() {
     const VALUE = Math.floor(Math.random() * 3);
@@ -13,51 +33,79 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    const choice = prompt().toLowerCase();
-    if (["rock", "paper", "scissors"].includes(choice)) {
-        return choice;
-    } else {
-        return "invalid choice";
-    }
-}
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log(`${humanChoice} ties ${computerChoice}`);
+        /*console.log(`${humanChoice} ties ${computerChoice}`);*/
+        result.textContent = `${humanChoice} ties ${computerChoice}`;
         return 0;
     } else if ((humanChoice === "rock" && computerChoice === "scissors") || 
     (humanChoice === "paper" && computerChoice === "rock") || 
     (humanChoice === "scissors" && computerChoice === "paper")) {
-        console.log(`${humanChoice} beats ${computerChoice}, you win!`);
+        /*console.log(`${humanChoice} beats ${computerChoice}, you win!`);*/
+        result.textContent = `${humanChoice} beats ${computerChoice}, you win!`;
+        humanScore++;
+        updateScore();
+        checkWinner();
         return 1;
     } else if ((humanChoice === "rock" && computerChoice === "paper") || 
     (humanChoice === "paper" && computerChoice === "scissors") || 
     (humanChoice === "scissors" && computerChoice === "rock")) {
-        console.log(`${humanChoice} loses to ${computerChoice}, you lose!`);
+        /*console.log(`${humanChoice} loses to ${computerChoice}, you lose!`);*/
+        result.textContent = `${humanChoice} loses to ${computerChoice}, you lose!`;
+        computerScore++;
+        updateScore();
+        checkWinner();
         return 2;
     }
 }
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    const rounds = 5
-    for (let i=0; i < rounds; i++) {
-        const score = playRound(getHumanChoice(), getComputerChoice());
-        if (score === 1) {
-            humanScore++;
-        } else if (score === 2) {
-            computerScore++;
-        }
-    }
-    if (humanScore > computerScore) {
-        console.log(`you win by a score of ${humanScore} to ${computerScore}`)
-    } else if (humanScore === computerScore) {
-        console.log(`you tie, the score was ${humanScore} to ${computerScore}`)
-    } else {
-        console.log(`you lose by a score of ${humanScore} to ${computerScore}`)
-    }
-}
 
-playGame();
+
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener("click", event=> {
+        playRound(event.target.value, getComputerChoice());
+    })
+})
+
+
+
+
+
+// /*this is unnecessary now*/
+// function getHumanChoice() {
+//     const choice = prompt().toLowerCase();
+//     if (["rock", "paper", "scissors"].includes(choice)) {
+//         return choice;
+//     } else {
+//         return "invalid choice";
+//     }
+// }
+
+
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     const rounds = 1;
+//     for (let i=0; i < rounds; i++) {
+//         const score = playRound(getHumanChoice(), getComputerChoice());
+//         if (score === 1) {
+//             humanScore++;
+//         } else if (score === 2) {
+//             computerScore++;
+//         }
+//     }
+//     if (humanScore > computerScore) {
+//         /*console.log(`you win by a score of ${humanScore} to ${computerScore}`);*/
+//         result.textContent = `you win by a score of ${humanScore} to ${computerScore}`;
+//     } else if (humanScore === computerScore) {
+//         /*console.log(`you tie, the score was ${humanScore} to ${computerScore}`);*/
+//         result.textContent = `you tie, the score was ${humanScore} to ${computerScore}`;
+//     } else {
+//         // console.log(`you lose by a score of ${humanScore} to ${computerScore}`);
+//         result.textContent = `you lose by a score of ${humanScore} to ${computerScore}`;
+//     }
+// }
